637df85fc23432be04be7de1c2f6258545b5401a
KeffSchwift <141948346+KeffSchwift@users.noreply.github.com>
2026-03-16 17:40:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.use('/bj', require('./api/bj'));
 app.use('/auto', require('./api/autos'));
-app.use('/armas', require('./api/armas')); 
-app.use('/time', require('./api/time'));
+app.use('/arma', require('./api/armas')); 
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
