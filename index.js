a80f678e2caa7d7c3fecd1c1a62b5ec6241c6d44
KeffSchwift <141948346+KeffSchwift@users.noreply.github.com>
2026-03-16 18:44:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/auto', require('./api/autos'));
-//app.use('/arma', require('./api/armas')); 
+app.use('/arma', require('./api/armas')); 
 
 
 app.listen(3000, () => {
