8b6805897e82fc17cf94af5bf4f9fdfd49c01d94
KeffSchwift <141948346+KeffSchwift@users.noreply.github.com>
2026-04-03 12:10:40 -0600

    Add route for La Liga API

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.use(express.json());
 app.use('/auto', require('./api/autos'));
 app.use('/arma', require('./api/armas')); 
 app.use('/gta', require('./api/gta')); 
+app.use('/laliga', require('./api/laliga'));
 
 
 app.listen(3000, () => {
