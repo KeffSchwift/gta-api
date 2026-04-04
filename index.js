90de0efcae4f9ff1dccb850f4223387523e4d93c
KeffSchwift <141948346+KeffSchwift@users.noreply.github.com>
2026-04-04 00:59:11 -0600

    Remove '/auto' route from Express app
    Removed the '/auto' route from the Express app.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.use('/auto', require('./api/autos'));
 app.use('/arma', require('./api/armas')); 
 app.use('/gta', require('./api/gta')); 
 app.use('/liga', require('./api/laliga'));
